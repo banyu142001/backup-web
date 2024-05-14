dfb699e93ac950e9826f104eebe948917b5a4e93
Bayu Gurium <bayuater@gmail.com>
2024-05-14 14:53:07 +0900

    menambahkan pembeda master dan fitru 1

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -17,7 +17,7 @@
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="fa-solid fa-gauge-high"></i>
                     </div>
-                    <span class="nav-link-text ms-1">Dashboard</span>
+                    <span class="nav-link-text ms-1">Dashboard Fitur 1</span>
                 </a>
             </li>
             <li class="nav-item">
