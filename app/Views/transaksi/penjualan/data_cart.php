bc0d3613b6af6805a489b0a384eb7bc2034e1676
Bayu Gurium <bayuater@gmail.com>
2024-05-25 17:15:13 +0900

    menambaahkan fitur edir data cart bagian 1 (menampilkan modal Update data)

--- app/Views/transaksi/penjualan/data_cart.php
+++ app/Views/transaksi/penjualan/data_cart.php
@@ -1,12 +1,12 @@
 <?php foreach ($data_cart as $cart) : ?>
     <tr>
-        <td class="fw-normal p-1" style="font-size:14px;"><?= $cart['nama_produk'] ?></td>
+        <td class="fw-normal p-1" style="font-size:15px;"> <strong><?= $cart['nama_produk'] ?></strong></td>
         <td class="fw-normal p-1" style="font-size:14px;"><?= number_format($cart['harga']) ?></td>
         <td class="fw-normal p-1" style="font-size:14px;"><?= $cart['qty'] ?></td>
         <td class="fw-normal p-1" style="font-size:14px;"><?= $cart['diskon'] ?></td>
         <td class="fw-normal p-1" style="font-size:14px;"><?= number_format($cart['total']) ?></td>
         <td class="align-middle text-center p-1" style="font-size:14px;">
-            <a href="" class="text-xs mx-1" <?= text_success ?> data-bs-toggle="modal" data-bs-target="#">
+            <a id="update_cart" class="text-xs mx-1 cursor-pointer" <?= text_success ?> data-bs-toggle="modal" data-bs-target="#modalUpdate" data-id_cart="<?= $cart['id_cart'] ?>" data-kode_produk="<?= $cart['kode_produk'] ?>" data-nama_produk="<?= $cart['nama_produk'] ?>" data-harga="<?= $cart['harga'] ?>" data-qty="<?= $cart['qty'] ?>" data-diskon="<?= $cart['diskon'] ?>" data-total="<?= $cart['total'] ?>">
                 Update
             </a>
             <a id="del_cart" data-id_cart="<?= $cart['id_cart'] ?>" class="text-xs cursor-pointer" <?= text_danger ?>>
--- app/Views/transaksi/penjualan/form.php
+++ app/Views/transaksi/penjualan/form.php
@@ -48,12 +48,63 @@
         </div>
     </div>
 </div>
+
+<!-- modal update cart -->
+<div class="modal fade" id="modalUpdate" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm border-0 shadow-none position-relative ">
+        <div class="modal-content rounded-1 shadow-none border-0">
+            <div class="modal-header p-0 py-1 px-3">
+                <p class="modal-title fw-bolder mt-2">Update Produk</p>
+                <span data-bs-dismiss="modal" aria-label="Close" class="cursor-pointer position-absolute top-2 start-100  translate-middle p-2"><i class="fas fa-times-circle bg-white rounded-circle border-0 text-danger" style="font-size: 25px;"></i></span>
+            </div>
+            <div class="modal-body table-responsive">
+                <div class="form-group mb-0">
+                    <label for="" class="my-0">Produk</label>
+                    <div class="row">
+                        <input type="hidden" id="id_cart">
+                        <div class="col-6">
+                            <input type="text" id="kode" class="form-control border form-control-sm" readonly>
+                        </div>
+                        <div class="col-6">
+                            <input type="text" id="nama" class="form-control border form-control-sm" readonly>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group mb-0">
+                    <label for="harga" class="my-0">Harga</label>
+                    <input type="number" id="harga1" class="form-control border form-control-sm">
+                </div>
+                <div class="form-group mb-0">
+                    <label for="jumlah" class="my-0">Jumlah</label>
+                    <input type="number" id="jumlah" class="form-control border form-control-sm">
+                </div>
+                <div class="form-group mb-0">
+                    <label for="total1" class="my-0">Total sebelum Diskon</label>
+                    <input type="number" id="total1" class="form-control border form-control-sm">
+                </div>
+                <div class="form-group mb-0">
+                    <label for="diskon_produk" class="my-0">Diskon per Produk</label>
+                    <input type="number" id="diskon_produk" class="form-control border form-control-sm">
+                </div>
+                <div class="form-group mb-0">
+                    <label for="total2" class="my-0">Total setelah Diskon</label>
+                    <input type="number" id="total2" class="form-control border form-control-sm">
+                </div>
+                <div class="form-group mb-0">
+                    <button class="btn btn-sm rounded-1 text-white w-100 mt-1" <?= btn_success ?> name="submit" type="submit">Simpan</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
 <script>
     $(document).ready(function() {
 
+        // jqyuery tamabh data cart belanja
         $(document).on('click', '#select', function() {
 
-            // ambil data dari button
+            // ambil data dari button search 
             const id_produk = $(this).data('id_produk');
             const kode_produk = $(this).data('kode_produk');
             const harga = $(this).data('harga');
@@ -65,6 +116,29 @@
             $('#harga').val(harga);
             $('#stok').val(stok);
             $('#modalCart').modal('hide');
+        })
+
+        // jquery update cart data
+        $(document).on('click', '#update_cart', function() {
+
+            // ambil data dari button update
+            const id_cart = $(this).data('id_cart');
+            const kode_produk = $(this).data('kode_produk');
+            const nama_produk = $(this).data('nama_produk');
+            const harga = $(this).data('harga');
+            const qty = $(this).data('qty');
+            const diskon = $(this).data('diskon');
+            const total = $(this).data('total');
+
+            // set value dari tiap inputan berdasarkan id
+            $('#id_cart').val(id_cart);
+            $('#kode').val(kode_produk);
+            $('#nama').val(nama_produk);
+            $('#harga1').val(harga);
+            $('#jumlah').val(qty);
+            $('#total1').val((harga * qty));
+            $('#diskon_produk').val(diskon);
+            $('#total2').val(total)
         })
 
         $(document).on('click', '#cart', function() {
