850215a01ed583a90edaa793bdc5fe1b28025f84
Bayu Gurium <bayuater@gmail.com>
2024-06-05 12:21:12 +0900

    Commit

--- app/Views/front-home/index.php
+++ app/Views/front-home/index.php
@@ -15,8 +15,8 @@
                 </h5>
                 <div class="position-relative w-100 mt-3">
                     <input class="form-control p-3 border rounded-pill w-100 ps-4 pe-5" type="text" placeholder="Your Email" />
-                    <button type="button" class="btn btn-info rounded-pill py-2 px-3 shadow-none position-absolute top-5 end-0 m-2">
-                        Get Pro
+                    <button type="button" class="btn btn-warning rounded-pill py-2 px-3 shadow-none position-absolute top-5 end-0 m-2">
+                        Tampilan
                     </button>
                 </div>
             </div>
